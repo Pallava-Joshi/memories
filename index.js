6ca58463c60f23afe5b5a3ebc0318fc248e7f44e
Pallava-Joshi <joshipallava@gmail.com>
2023-02-10 22:53:06 +0530

    memories

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+import userRouter from "./routes/user.js";
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+app.use("/user", userRouter);
+
+// ... other imports
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+// ... other app.use middleware
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+const CONNECTION_URL =
+  "mongodb+srv://joshipallava:7dZEYzYCctsgwDu@cluster0.n1mdrue.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
